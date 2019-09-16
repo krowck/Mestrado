7e9424794bc150c1f037853c89e3faa98b4d86bd
Krowck <gabrieldominicoo@gmail.com>
2019-09-16 17:21:48 -0300

    script updated

---
+++ NDBSCANjDE-Archive/run1-20.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+declare -a func=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+for j in {0..19}
+do
+	python3 ndbjde.py -acc 0.001 -a 1 -flag 0 -r 5 -p 150 -f "${func[$j]}" &
+done
