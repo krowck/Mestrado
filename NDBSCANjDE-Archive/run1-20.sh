d1c0d16bfb97e2c00e36191cf7db0e561f537192
Gabriel <gabrieldominicoo@gmail.com>
2019-09-17 13:23:19 +0000

    Some tests

--- NDBSCANjDE-Archive/run1-20.sh
+++ NDBSCANjDE-Archive/run1-20.sh
@@ -2,5 +2,5 @@
 declare -a func=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
 for j in {0..19}
 do
-	python3 ndbjde.py -acc 0.001 -a 1 -flag 0 -r 5 -p 150 -f "${func[$j]}" &
+	python3 ndbjde.py -acc 0.001 -a 1 -flag 0 -r 10 -p 150 -f "${func[$j]}" &
 done
